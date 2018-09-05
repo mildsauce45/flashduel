e264bb745edb9dead6d88861af2d6d1e7de7fd5e
Jared Pinchot <jmpinchot@gmail.com>
2018-09-05 16:57:16 -0700

    added push action and its tests

--- src/main/kotlin/models/Card.kt
+++ src/main/kotlin/models/Card.kt
@@ -4,4 +4,9 @@ data class Card(val value: Int) {
     companion object {
         const val MAX_VALUE = 5
     }
+
+    init {
+        if (value > MAX_VALUE)
+            throw IllegalArgumentException("Card value exceeds max value of $MAX_VALUE")
+    }
 }
